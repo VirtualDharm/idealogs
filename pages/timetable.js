a200efc486101672fba0c0c44c514f2abeb40d58
VirtualDharm <mdharm4air.fm@gmail.com>
2024-11-11 17:01:24 +0530

    update

--- pages/timetable.js
+++ pages/timetable.js
@@ -30,7 +30,11 @@ export default function Timetable() {
 
             <div style={styles.schedule}>
                 <div style={{ ...styles.timeBlock, ...styles.study }}>
-                    <h3 style={styles.timeBlockHeading}>1:00 AM - 6:30 AM</h3>
+                    <h3 style={styles.timeBlockHeading}>1:00 AM - 3:00AM</h3>
+                    <p style={styles.timeBlockText}>Apply</p>
+                </div>
+                <div style={{ ...styles.timeBlock, ...styles.study }}>
+                    <h3 style={styles.timeBlockHeading}>3:00 AM - 6:30AM</h3>
                     <p style={styles.timeBlockText}>DSA</p>
                 </div>
                 <div style={{ ...styles.timeBlock, ...styles.physical }}>
@@ -38,11 +42,7 @@ export default function Timetable() {
                     <p style={styles.timeBlockText}>Gym</p>
                 </div>
                 <div style={{ ...styles.timeBlock, ...styles.study }}>
-                    <h3 style={styles.timeBlockHeading}>8:00 AM - 10:00 AM</h3>
-                    <p style={styles.timeBlockText}>Apply</p>
-                </div>
-                <div style={{ ...styles.timeBlock, ...styles.study }}>
-                    <h3 style={styles.timeBlockHeading}>10:00 AM - 6:00 PM</h3>
+                    <h3 style={styles.timeBlockHeading}>8:00 AM - 6:00 PM</h3>
                     <p style={styles.timeBlockText}>Development</p>
                 </div>
                 <div style={{ ...styles.timeBlock, ...styles.physical, opacity: 0.77 }}>
